9c81bae7321956f30d323a324773ba0d87b18756
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-09 23:06:52 -0500

    added laundry_in_building and formatted body of request

--- getInfo.js
+++ getInfo.js
@@ -1,24 +1,63 @@
 if (location.href.match("streeteasy.com/building/")) {
-  const listing = {}
-  listing.street_address_one = document.querySelector("a.incognito").innerText
+  const listing = {
+    dishwasher: false,
+    guarantors_accepted: false,
+    outdoor_space: false,
+    laundry_in_unit: false,
+    doorman: false,
+    package_handling: false,
+    elevator: false,
+    gym: false,
+    laundry_in_building: false,
+  }
+  listing.street_address = document.querySelector("a.incognito").innerText
+  console.log(listing.street_address);
   let loctn = document.querySelector("div.backend_data.BuildingInfo-item > span.Text").innerText.split(" ")
   listing.city = loctn[0].match(/(\w+)/)[0]
   listing.state = loctn[1]
   listing.zip_code = parseInt(loctn[2], 10)
+  listing.neighborhood = Array.from(document.querySelectorAll("div.details_info > span.nobreak")).find(el => el.innerText.includes("in ")).innerText.split("in ")[1]
   listing.price = parseInt(document.querySelector("div.price").innerText.split("$")[1].split(" FOR RENT")[0].split(",").join(""), 10)
   listing.no_fee = !!document.querySelector("div.status.nofee")
   const details = Array.from(document.querySelectorAll("span.detail_cell"))
   listing.num_beds = parseInt(details.find(el => el.innerText.includes("bed")).innerText.split(" bed")[0], 10)
   listing.num_bath = parseInt(details.find(el => el.innerText.includes("bath")).innerText.split(" bath")[0], 10)
   const amenities = Array.from(document.querySelectorAll("ul.AmenitiesBlock-list li")).map(el => el.innerText)
-  // document.querySelector("div.backend_data.BuildingInfo-item > span.Text").text.gsub(/\n|,/,"").split(" ")[0]
-  // document.querySelector("div.backend_data.BuildingInfo-item > span.Text").text.gsub(/\n|,/,"").split(" ")[1]
-  // document.querySelector("div.backend_data.BuildingInfo-item > span.Text").text.gsub(/\n|,/,"").split(" ")[-1].to_i
-  // document.querySelector("div.price").children[2].text.gsub(/\ |\n|\$|\,/, "").to_i
-  // document.querySelector("span.detail_cell").find {|el| el.text.include?("bed")}.text.split(" ").first.to_i
-  // document.querySelector("span.detail_cell").find {|el| el.text.include?("bath")}.text.split(" ").first.to_i
-  // document.querySelector("div.Description-block.jsDescriptionExpanded").inner_html
-  listing.amenties = amenities
+
+  amenities.forEach(am => {
+    switch (am) {
+      case "Dishwasher":
+        listing.dishwasher = true
+        break;
+      case "Guarantors Accepted":
+        listing.guarantors_accepted = true
+        break;
+      case "Public or Private Outdoor Space":
+        listing.outdoor_space = true
+        break;
+      case "Washer/Dryer In-Unit":
+        listing.laundry_in_unit = true
+        break;
+      case "Laundry in Building":
+        listing.laundry_in_building = true
+        break;
+      case "Doorman":
+        listing.doorman = true
+        listing.package_handling = true
+        break;
+      case "Package Room":
+        listing.package_handling = true
+        break;
+      case "Elevator":
+        listing.elevator = true
+        break;
+      case "Gym":
+        listing.gym = true
+        break;
+      default:
+        break;
+    }
+  })
   listing.url = location.href
   listing
 }
--- index.js
+++ index.js
@@ -9,6 +9,73 @@ document.addEventListener("DOMContentLoaded", () => {
           chrome.tabs.executeScript(tabs[tab].id, {file: "getInfo.js"}, function(response) {
             if (response) {
               const listing = response[0]
+              console.log(listing);
+              const form = document.createElement("form")
+              form.innerHTML = (`
+                <label for="street_address">Address</label>
+                <input type="text" value="${listing.street_address}" name="street_address" /><br />
+                <label for="city">City</label>
+                <input type="text" value="${listing.city}" name="city" /><br />
+                <label for="state">State</label>
+                <input type="text" value="${listing.state}" name="state" /><br />
+                <label for="zip_code">Zip Code</label>
+                <input type="text" value="${listing.zip_code}" name="zip_code" /><br />
+                <label for="neighborhood">Neighborhood</label>
+                <input type="text" value="${listing.neighborhood}" name="neighborhood" /><br />
+                <label for="price">Price</label>
+                <input type="number" value="${listing.price}" name="price" /><br />
+                <label for="num_beds">Beds:</label>
+                <input type="number" value="${listing.num_beds}" name="num_beds" /><br />
+                <label for="num_bath">Baths</label>
+                <input type="number" value="${listing.num_bath}" name="num_bath" step="0.5"/><br />
+                <label for="no_fee">No Fee?</label>
+                <input type="checkbox" ${listing.no_fee && "checked"} name="no_fee" /><br />
+                <label for="dishwasher">Dishwasher</label>
+                <input type="checkbox" ${listing.dishwasher && "checked"} name="dishwasher" /><br />
+                <label for="guarantors_accepted">Guarantors Accepted?</label>
+                <input type="checkbox" ${listing.guarantors_accepted && "checked"} name="guarantors_accepted" /><br />
+                <label for="outdoor_space">Outdoor Space?</label>
+                <input type="checkbox" ${listing.outdoor_space && "checked"} name="outdoor_space" /><br />
+                <label for="laundry_in_unit">Laundry In Unit?</label>
+                <input type="checkbox" ${listing.laundry_in_unit && "checked"} name="laundry_in_unit" /><br />
+                <label for="laundry_in_building">Laundry In Building?</label>
+                <input type="checkbox" ${listing.laundry_in_building && "checked"} name="laundry_in_building" /><br />
+                <label for="doorman">Doorman?</label>
+                <input type="checkbox" ${listing.doorman && "checked"} name="doorman" /><br />
+                <label for="package_handling">Package Handling?</label>
+                <input type="checkbox" ${listing.package_handling && "checked"} name="package_handling" /><br />
+                <label for="elevator">Elevator?</label>
+                <input type="checkbox" ${listing.elevator && "checked"} name="elevator" /><br />
+                <label for="gym">Gym?</label>
+                <input type="checkbox" ${listing.gym && "checked"} name="gym" /><br />
+                <input type="submit" />
+              `)
+              document.querySelector("body").append(form)
+              form.addEventListener("submit", (e) => {
+                e.preventDefault()
+                const body = {
+                  street_address: e.target.street_address.value,
+                  city: e.target.city.value,
+                  state: e.target.state.value,
+                  zip_code: e.target.zip_code.value,
+                  neighborhood: e.target.neighborhood.value,
+                  price: e.target.price.value,
+                  num_beds: e.target.num_beds.value,
+                  num_bath: e.target.num_bath.value,
+                  no_fee: e.target.no_fee.checked,
+                  dishwasher: e.target.dishwasher.checked,
+                  guarantors_accepted: e.target.guarantors_accepted.checked,
+                  outdoor_space: e.target.outdoor_space.checked,
+                  laundry_in_unit: e.target.laundry_in_unit.checked,
+                  laundry_in_building: e.target.laundry_in_building.checked,
+                  doorman: e.target.doorman.checked,
+                  package_handling: e.target.package_handling.checked,
+                  elevator: e.target.elevator.checked,
+                  gym: e.target.gym.checked,
+                  url: listing.url
+                }
+                console.log(body);
+              })
             }
           });
         }
